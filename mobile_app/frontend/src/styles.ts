8f81e35c5d1addf9352b13d7d95c820a10116efb
pemiinem <190139323@aluno.unb.br>
2024-08-27 22:19:34 -0300

    chore: update iOS Podfile and package.json dependencies

---
+++ mobile_app/frontend/src/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default styles;
